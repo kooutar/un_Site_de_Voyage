8a35386e628066e7ff2b02b8f981a9ba85405a64
kaoutar laajil <kawtarlaajil7@gmail.com>
2024-12-25 22:06:35 +0100

    create class database

---
+++ dataBase/db.php
@@ -0,0 +1,38 @@
+<?php 
+//  $servername = "192.168.8.127";
+//  $username = "root";
+//  $password = "";
+//  $dateBaseName="voyage2";
+
+ class dataBase {
+  private static $instance = null;//type dataBase
+  private $connection;//type PDO
+
+  private function __construct() {
+      $this->connection = new PDO("mysql:host=localhost;dbname=voyage2", "root", "");
+  }
+
+  public static function getInstance() {
+      if (self::$instance == null) {
+        try {
+          self::$instance = new dataBase();
+          self::$instance->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          echo "Connected successfully";
+        } catch (PDOException $e) {
+          echo "Connection failed: " . $e->getMessage();
+        }
+          
+      }
+      return self::$instance;
+  }
+
+  public function getConnection() {
+      return $this->connection;
+  }
+
+ 
+}
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php 
+   require_once('dataBase/db.php');
+   $db1 = dataBase::getInstance();
+   $conn1 = $db1->getConnection();
+?>
